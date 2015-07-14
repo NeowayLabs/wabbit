e53c85d52973c7277e86873c1e6fd5b5dff799dd
Tiago Natel <tiago.natel@neoway.com.br>
2015-07-14 10:09:36 -0300

    [CHANGE] update tests to use conjure library

--- dial_test.go
+++ dial_test.go
@@ -1,148 +1,35 @@
 package amqputil
 
 import (
-	"bufio"
-	"fmt"
-	"net"
 	"os"
 	"runtime"
 	"testing"
 	"time"
 
-	"github.com/fsouza/go-dockerclient"
+	"github.com/tiago4orion/conjure"
 )
 
 var (
-	dockerClient *docker.Client
+	rabbitctl *conjure.RabbitMQ
 )
 
 func TestMain(m *testing.M) {
 	var err error
 
-	// Start the amqp backend on test startup
-	endpoint := "unix:///var/run/docker.sock"
-	dockerClient, err = docker.NewClient(endpoint)
+	ctnClient, err := conjure.NewClient()
 
-	if err != nil {
-		fmt.Printf("Error connecting to docker daemon...")
-		os.Exit(1)
+	if err != nil || ctnClient == nil {
+		panic(err.Error())
 	}
 
-	// remove any previously executed rabbitmq container with name
-	// rabbitmq-tests
-	rmRabbit()
+	rabbitctl = conjure.NewRabbitMQ(ctnClient)
 
 	// Execute the tests
 	status := m.Run()
 
 	// remove the backend container
-	rmRabbit()
-
+	rabbitctl.Remove()
 	os.Exit(status)
-}
-func rmRabbit() {
-	rmCtnOpt := docker.RemoveContainerOptions{
-		ID:            "rabbitmq-tests",
-		RemoveVolumes: true,
-		Force:         true,
-	}
-
-	_ = dockerClient.RemoveContainer(rmCtnOpt)
-}
-
-func pullRabbit() error {
-	pullOpts := docker.PullImageOptions{
-		Repository:   "rabbitmq",
-		Tag:          "latest",
-		OutputStream: os.Stdout,
-	}
-
-	err := dockerClient.PullImage(pullOpts, docker.AuthConfiguration{})
-
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// runRabbit execute a container with rabbitmq daemon
-// If docker fail to create the container, then try to pull the
-// last version of rabbitmq image from registry. If fail again,
-// then returns error.
-// When rabbitmq is successfully started, block until it finish the
-// setup and is ready to accept connections.
-func runRabbit() (*docker.Container, error) {
-	var rabbitCtn *docker.Container
-
-	createContainer := func() (*docker.Container, error) {
-		opts := docker.CreateContainerOptions{
-			Name: "rabbitmq-tests",
-			Config: &docker.Config{
-				Image: "rabbitmq",
-				ExposedPorts: map[docker.Port]struct{}{
-					"5672/tcp": {},
-				},
-			},
-			HostConfig: &docker.HostConfig{
-				PortBindings: map[docker.Port][]docker.PortBinding{
-					"5672/tcp": []docker.PortBinding{docker.PortBinding{HostPort: "5672"}}},
-				PublishAllPorts: true,
-				Privileged:      false,
-			},
-		}
-
-		rabbitCtn, err := dockerClient.CreateContainer(opts)
-
-		return rabbitCtn, err
-	}
-
-	rabbitCtn, err := createContainer()
-
-	if err != nil {
-		err = pullRabbit()
-
-		if err != nil {
-			return nil, err
-		}
-
-		rabbitCtn, err = createContainer()
-
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	err = dockerClient.StartContainer(rabbitCtn.ID, nil)
-
-	if err != nil {
-		return nil, err
-	}
-
-	// block until rabbitmq can accept connections on port 5672
-	waitRabbit()
-
-	return rabbitCtn, nil
-}
-
-// waitRabbit blocks until rabbitmq can accept connections on
-// localhost:5672
-func waitRabbit() {
-dial:
-	conn, err := net.Dial("tcp", "localhost:5672")
-	if err != nil {
-		time.Sleep(500 * time.Millisecond)
-		goto dial
-	}
-
-	fmt.Fprintf(conn, "AMQP%00091")
-	_, err = bufio.NewReader(conn).ReadString('\n')
-
-	if err != nil && err.Error() != "EOF" {
-		conn.Close()
-		time.Sleep(500 * time.Millisecond)
-		goto dial
-	}
 }
 
 // TestDial test a simple connection to rabbitmq.
@@ -157,7 +44,7 @@ func TestDial(t *testing.T) {
 		return
 	}
 
-	_, err = runRabbit()
+	err = rabbitctl.Run()
 	if err != nil {
 		t.Error(err)
 	}
@@ -169,11 +56,11 @@ func TestDial(t *testing.T) {
 		return
 	}
 
-	rmRabbit()
+	rabbitctl.Remove()
 }
 
 func TestAutoRedial(t *testing.T) {
-	rabbitCtn, err := runRabbit()
+	err :=rabbitctl.Run()
 
 	if err != nil {
 		t.Errorf("Failed to start rabbitmq: %s", err.Error())
@@ -190,7 +77,7 @@ func TestAutoRedial(t *testing.T) {
 
 	var success = false
 	redialErrors := make(chan error)
-	conn.AutoRedial(redialErrors, func () {
+	conn.AutoRedial(redialErrors, func() {
 		// this function should be executed if successfully reconnected
 		// here we can recover the application state after reconnect  (if needed)
 		success = true
@@ -204,13 +91,13 @@ func TestAutoRedial(t *testing.T) {
 		}
 	}()
 
-	dockerClient.StopContainer(rabbitCtn.ID, 3)
+	rabbitctl.Stop()
 
 	// concurrently starts the rabbitmq after 1 second
 	go func() {
 		time.Sleep(1 * time.Second)
 
-		err := dockerClient.StartContainer(rabbitCtn.ID, nil)
+		err := rabbitctl.Start()
 
 		if err != nil {
 			t.Error(err)
