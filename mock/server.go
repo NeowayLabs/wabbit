b7adc64454b2fa7b1d59f945df4dc87971213d00
Tiago <tiago.natel@neoway.com.br>
2015-09-03 19:12:26 -0300

    mock/server.go: started server implementation

---
+++ mock/server.go
@@ -0,0 +1,74 @@
+package mock
+
+import (
+	"errors"
+	"sync"
+)
+
+var (
+	servers map[string]*AMQPServer
+	mu      *sync.Mutex
+)
+
+func init() {
+	servers = make(map[string]*AMQPServer)
+
+	mu = &sync.Mutex{}
+}
+
+// AMQPServer is a fake AMQP server
+type AMQPServer struct {
+	mu      *sync.Mutex
+	running bool
+	amqpuri string
+}
+
+// NewServer returns a new fake amqp server
+func newServer() *AMQPServer {
+	return &AMQPServer{
+		mu: &sync.Mutex{},
+	}
+}
+
+// Start a new AMQP server fake-listening on host:port
+func (s *AMQPServer) Start(amqpuri string) error {
+	s.amqpuri = amqpuri
+	s.running = true
+
+	return nil
+}
+
+// Stop the fake server
+func (s *AMQPServer) Stop() error {
+	s.running = false
+	return nil
+}
+
+// NewServer starts a new fake server
+func NewServer(amqpuri string) *AMQPServer {
+	var amqpServer *AMQPServer
+
+	mu.Lock()
+	defer mu.Unlock()
+
+	amqpServer = servers[amqpuri]
+	if amqpServer == nil {
+		amqpServer = newServer(amqpuri)
+		servers[amqpuri] = amqpServer
+	}
+
+	return amqpServer
+}
+
+func connect(amqpuri string) (*AMQPServer, error) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	amqpServer := servers[amqpuri]
+
+	if amqpServer == nil {
+		return nil, errors.New("Network unreachable")
+	}
+
+	return amqpServer, nil
+}
