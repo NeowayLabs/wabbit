e05bf53888a129ef586b4bdedce21c25d4281d69
Tiago <tiago.natel@neoway.com.br>
2015-09-08 18:17:07 -0300

    improved tests

--- mock/server/exchange.go
+++ mock/server/exchange.go
@@ -8,14 +8,22 @@ type Exchange interface {
 }
 
 type TopicExchange struct {
+	name     string
 	bindings map[string]*Queue
+}
+
+func NewTopicExchange(name string) *TopicExchange {
+	return &TopicExchange{
+		name:     name,
+		bindings: make(map[string]*Queue),
+	}
 }
 
 func (t *TopicExchange) addBinding(route string, q *Queue) {
 	t.bindings[route] = q
 }
 
-func (t *TopicExchange) route(route string, message []byte) (*Queue, error) {
+func (t *TopicExchange) route(route string, _ []byte) (*Queue, error) {
 	for bname, q := range t.bindings {
 		if topicMatch(bname, route) {
 			return q, nil
@@ -23,4 +31,29 @@ func (t *TopicExchange) route(route string, message []byte) (*Queue, error) {
 	}
 
 	return nil, fmt.Errorf("Route '%s' doesn't match any routing-key", route)
+}
+
+type DirectExchange struct {
+	name     string
+	bindings map[string]*Queue
+}
+
+func NewDirectExchange(name string) *DirectExchange {
+	return &DirectExchange{
+		name:     name,
+		bindings: make(map[string]*Queue),
+	}
+}
+
+func (d *DirectExchange) addBinding(route string, q *Queue) {
+	d.bindings[route] = q
+}
+
+func (d *DirectExchange) route(route string, _ []byte) (*Queue, error) {
+	if q, ok := d.bindings[route]; ok {
+		return q, nil
+	}
+
+	return nil, fmt.Errorf("No bindings to route: %s", route)
+
 }
--- mock/server/vhost.go
+++ mock/server/vhost.go
@@ -29,7 +29,12 @@ func NewVHost(name string) *VHost {
 
 func (v *VHost) createDefaultExchanges() {
 	exchs := make(map[string]Exchange)
-	exchs["amqp.topic"] = &TopicExchange{}
+	exchs["amq.topic"] = &TopicExchange{}
+	exchs["amq.direct"] = &DirectExchange{}
+	exchs[""] = &DirectExchange{
+		name: "amq.direct",
+	}
+
 	v.exchanges = exchs
 }
 
@@ -50,16 +55,38 @@ func (v *VHost) Cancel(consumer string, noWait bool) error {
 }
 
 func (v *VHost) ExchangeDeclare(name, kind string, opt amqputil.Option) error {
+	if _, ok := v.exchanges[name]; ok {
+		// TODO: We need review this. If the application is trying to re-create an exchange
+		// using other options we shall not return NIL because this indicates success,
+		// but we didn't declared anything.
+		// The AMQP 0.9.1 spec says nothing about that. It only says that AMQP uses the
+		// "declare" concept instead of the "create" concept. If something is already
+		// declared it's no problem...
+		return nil
+	}
+
+	switch kind {
+	case "amq.topic":
+		v.exchanges[name] = NewTopicExchange(name)
+	case "amq.direct":
+		v.exchanges[name] = NewDirectExchange(name)
+	default:
+		return fmt.Errorf("Invalid exchange type: %s", kind)
+	}
+
 	return nil
 }
 
 func (v *VHost) QueueDeclare(name string, args amqputil.Option) (amqputil.Queue, error) {
-	q := &Queue{}
+	q := &Queue{
+		name: name,
+	}
+
 	v.queues[name] = q
 	return q, nil
 }
 
-func (v *VHost) QueueBind(name, key, exchange string, opt amqputil.Option) error {
+func (v *VHost) QueueBind(name, key, exchange string, _ amqputil.Option) error {
 	var (
 		exch Exchange
 		q    *Queue
