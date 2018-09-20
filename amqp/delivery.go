a09c7177de0f9ca54fec78571fab93670c74bf42
Ali Reza <alirezayahya@gmail.com>
2018-09-20 17:17:48 +0200

    add RoutingKey on delivery struct

--- amqp/delivery.go
+++ amqp/delivery.go
@@ -28,3 +28,7 @@ func (d *Delivery) ConsumerTag() string {
 func (d *Delivery) MessageId() string {
 	return d.Delivery.MessageId
 }
+
+func (d *Delivery) RoutingKey() string {
+	return d.Delivery.RoutingKey
+}
